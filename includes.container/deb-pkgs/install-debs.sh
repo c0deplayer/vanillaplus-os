13402ecc5c74f4100b92e4e11b3c6a81abfae90c
Jakub Zenon Kujawa <ZK_Jakub@proton.me>
2024-02-17 19:50:06 +0100

    style: small code formatting changes [skip ci]

--- includes.container/deb-pkgs/install-debs.sh
+++ includes.container/deb-pkgs/install-debs.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+
 set -e
+
 for file in /deb-pkgs/*; do
     if [ ! -f "$file" ]; then
         continue
     fi
 
     extension=${file##*.}
-    if [ $extension != "deb" ]; then
+
+    if [ "$extension" != "deb" ]; then
         continue
     fi
 
     echo "installing $file"
-
-    apt-get install -y $file
+    apt-get install -y "$file"
 done
